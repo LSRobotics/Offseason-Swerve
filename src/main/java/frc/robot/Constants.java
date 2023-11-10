96cbce2b0e37abf0196453f63d01f86cda902f8d
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2023-11-10 15:49:23 -0500

    changed test motor angles

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -95,16 +95,22 @@ public final class Constants {
         /* Module Specific Constants */
         /* Front Left Module - Module 0 */
         public static final class Mod0 { //TODO: SPECIFY THE CAN IDS AND SHTUFF
+            
+            //LEFT FRONT MODULE
+
             public static final int driveMotorID = 11;
             public static final int angleMotorID = 10;
             public static final int canCoderID = 25;
-            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(0.0);
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-44.7);
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
         }
 
         /* Front Right Module - Module 1 */
         public static final class Mod1 { //TODO: This must be tuned to specific robot
+            
+            //RIGHT FRONT MODULE
+            
             public static final int driveMotorID = 5;
             public static final int angleMotorID = 4;     // 0.03
             public static final int canCoderID = 27;
@@ -115,6 +121,9 @@ public final class Constants {
         
         /* Back Left Module - Module 2 */
         public static final class Mod2 { //TODO: This must be tuned to specific robot
+            
+            //LEFT BACK MODULE
+            
             public static final int driveMotorID = 19;
             public static final int angleMotorID = 18;
             public static final int canCoderID = 26;
@@ -125,6 +134,9 @@ public final class Constants {
 
         /* Back Right Module - Module 3 */
         public static final class Mod3 { //TODO: This must be tuned to specific robot
+            
+            // RIGHT BACK MODULE
+            
             public static final int driveMotorID = 3;
             public static final int angleMotorID = 2;
             public static final int canCoderID = 24;
